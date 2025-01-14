8f427fc9710a87ccce851587eebc9a1864947f6c
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-01-14 05:39:09 -0500

    feat: programma non parallelo continua

--- a.c
+++ a.c
@@ -1,19 +1,28 @@
+//la matrice è di float in realtà
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-int **allocaMatrice(int dimensione) {
-    int **matrice = malloc(dimensione * sizeof(int *));
-    for (int i = 0; i < dimensione; i++) matrice[i] = malloc(dimensione * sizeof(int));
+int **alloca_inizializza_matrice(char *argv[], int dimensione) {
+	int dimensione_risultato = dimensione / 2 + 2;
+    int **matrice = malloc(dimensione_risultato * sizeof(int *));
+    for (int i = 0; i < dimensione_risultato; i++) matrice[i] = malloc(dimensione_risultato * sizeof(int));
+    for (int i = 0; i < dimensione_risultato; i++) matrice[i][0] = matrice[0][i] = 0;
+    int c = 3;
+    for (int i = 1; i < dimensione_risultato; i++) {
+    	for (int j = 1; j < dimensione_risultato; j++) matrice[i][j] = atoi(argv[c++]);
+    	c = 3 + dimensione * i;
+    }
     return matrice;
 }
 
-void deallocaMatrice(int **matrice, int dimensione) {
+void dealloca_matrice(int **matrice, int dimensione) {
     for (int i = 0; i < dimensione; i++) free(matrice[i]);
     free(matrice);
 }
 
-void stampaMatrice(int **matrice, int dimensione) {
+void stampa_matrice(int **matrice, int dimensione) {
     for (int i = 0; i < dimensione; i++) {
         for (int j = 0; j < dimensione; j++) printf("%d\t", matrice[i][j]);
         printf("\n");
@@ -21,9 +30,15 @@ void stampaMatrice(int **matrice, int dimensione) {
     printf("\n");
 }
 
-void conferma_media(int somma, int i, int j) {
+void conferma_media(int **matrice, int somma, int i, int j) {
 	 float media = somma / 9.0;
-     printf("Elemento [%d][%d] -> Media 3x3: %.2f\n", i, j, media);
+     printf("Elemento [%d][%d]: %d -> Media 3x3: %.2f\n", i, j, matrice[i][j], media);
+}
+
+int somma_intorno_prima(int **matrice, int somma_prima, int i, int j) {
+	int somma = 0;
+	for (int ik = -1; ik < 2; ik++) for (int jk = -1; jk < 2; jk++) somma += matrice[i + ik][j + jk];
+	return somma;
 }
 
 int somma_intorno_scorrimento(int **matrice, int somma_prima, int i_prima, int j_prima, int i_dopo, int j_dopo) {
@@ -42,15 +57,15 @@ int somma_intorno_scorrimento(int **matrice, int somma_prima, int i_prima, int j
 void media_intorno_processo(int **matrice, int righe, int colonne) {
 	int somma = 0;
     for (int i = 1; i < righe - 1; i++) {
-    	printf("Q");
         if (i % 2 == 1) for (int j = 1; j < colonne - 1; j++) {
-			if (j == 1) somma = somma_intorno_scorrimento(matrice, somma, i - 1, j, i, j);
-			else somma = somma_intorno_scorrimento(matrice, somma, i, j + 1, i, j);
-			conferma_media(somma, i, j);
+        	if (i == 1 && j == 1) somma = somma_intorno_prima(matrice, somma, i, j);
+			else if (j == 1) somma = somma_intorno_scorrimento(matrice, somma, i - 1, j, i, j);
+			else somma = somma_intorno_scorrimento(matrice, somma, i, j - 1, i, j);
+			conferma_media(matrice, somma, i, j);
         } else for (int j = colonne - 2; j > 0; j--) {
 		    if (j == colonne - 2) somma = somma_intorno_scorrimento(matrice, somma, i - 1, j, i, j);
-			else somma = somma_intorno_scorrimento(matrice, somma, i, j - 1, i, j);
-			conferma_media(somma, i, j);
+			else somma = somma_intorno_scorrimento(matrice, somma, i, j + 1, i, j);
+			conferma_media(matrice, somma, i, j);
         }
     }
 }
@@ -74,16 +89,12 @@ int main(int argc, char *argv[]) {
         printf("Numero di elementi insufficiente per una matrice di dimensione %dx%d.\n", dimensione, dimensione);
         return 1;
     }
-
+    
 	int dimensione_risultato = dimensione / 2;
-    int **A = allocaMatrice(dimensione);
-    int k = 3;
-    for (int i = 0; i < dimensione; i++) for (int j = 0; j < dimensione; j++) A[i][j] = atoi(argv[k++]);
+    int **A = alloca_inizializza_matrice(argv, dimensione);
 
-    printf("Matrice %dx%d:\n", dimensione, dimensione);
-    stampaMatrice(A, dimensione);
-    printf("Matrice in alto a sinistra%dx%d:\n", dimensione_risultato, dimensione_risultato);
-    stampaMatrice(A, dimensione_risultato);
+    printf("Matrice in alto a sinistra %dx%d con bordo aggiunto:\n", dimensione_risultato, dimensione_risultato);
+    stampa_matrice(A, dimensione_risultato + 2);
     
     int colonne = thread > dimensione_risultato ? dimensione_risultato : sqrt(thread);
     int dimensione_colonna = dimensione_risultato / colonne;
@@ -95,9 +106,15 @@ int main(int argc, char *argv[]) {
     printf("Colonne: %d %d %d\n", colonne, dimensione_colonna, colonne_grandi);
     printf("Righe: %d %d %d\n", righe, dimensione_riga, righe_grandi);
     
-    media_intorno_processo(A, dimensione_risultato, dimensione_risultato);
     
-    deallocaMatrice(A, dimensione);
+    
+    
+    
+    
+    
+    media_intorno_processo(A, dimensione_risultato + 2, dimensione_risultato + 2);
+    
+    dealloca_matrice(A, dimensione_risultato + 2);
 
     return 0;
 }
