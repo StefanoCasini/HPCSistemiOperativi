0ef113ab149540ae6dea83272abe8e99fe5f551c
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-01-16 07:03:28 -0500

    openmi fine

--- matrixAVG_mpi.c
+++ matrixAVG_mpi.c
@@ -1,80 +1,46 @@
-//la matrice è di float in realtà
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include <mpi.h>
+#include <omp.h>
 
-
-float **alloca_inizializza_matrice(int dimensione) {
-	int dimensione_risultato = dimensione / 2 + 2;
-    float **matrice = malloc(dimensione_risultato * sizeof(int *));
-    for (int i = 0; i < dimensione_risultato; i++) matrice[i] = malloc(dimensione_risultato * sizeof(float));
-    for (int i = 0; i < dimensione_risultato; i++) matrice[i][0] = matrice[0][i] = 0.0;
-    for (int i = 1; i < dimensione_risultato; i++) {
-    	for (int j = 1; j < dimensione_risultato; j++) matrice[i][j] = (float) rand() / RAND_MAX;
-    }
+float **create_matrix(int dim) {
+    float **matrice = malloc(dim * sizeof(float *));
+    for (int i = 0; i < dim; i++) matrice[i] = malloc(dim * sizeof(float));
+    for (int i = 0; i < dim; i++) matrice[i][0] = matrice[0][i] = 0.0;
+    for (int i = 1; i < dim; i++) for (int j = 1; j < dim; j++) matrice[i][j] = (float) rand() / RAND_MAX;
     return matrice;
 }
 
-void dealloca_matrice(float **matrice, int dimensione) {
-    for (int i = 0; i < dimensione; i++) free(matrice[i]);
-    free(matrice);
+void free_matrix(float **m, int dim) {
+    for (int i = 0; i < dim; i++) free(m[i]);
+    free(m);
 }
 
-void stampa_matrice(float **matrice, int dimensione) {
-    for (int i = 0; i < dimensione; i++) {
-        for (int j = 0; j < dimensione; j++) printf("%f\t", matrice[i][j]);
+void show_matrix(float **m, int dim) {
+    for (int i = 0; i < dim; i++) {
+        for (int j = 0; j < dim; j++) printf("%f\t", m[i][j]);
         printf("\n");
     }
     printf("\n");
 }
 
-float **dividi_matrice(float **matrice, int i_inizio, int i_fine, int j_inizio, int j_fine) {
-	float **divisa = malloc((i_fine - i_inizio + 3) * sizeof(float *));
-	for (int i = 0; i < i_fine - i_inizio + 3; i++) divisa[i] = malloc((j_fine - j_inizio + 3) * sizeof(float));
-	for (int i = i_inizio - 1; i <= i_fine + 1; i++) for (int j = j_inizio - 1; j <= j_fine + 1; j++)
-		divisa[i - i_inizio + 1][j - j_inizio + 1] = matrice[i][j];
-	return divisa;
+float first_sum_adj(float **m, float old_sum, int i, int j) {
+	float sum = 0;
+	for (int ik = -1; ik < 2; ik++) for (int jk = -1; jk < 2; jk++) sum += m[i + ik][j + jk];
+	return sum;
 }
 
-void conferma_media(float **matrice, float somma, int i, int j) {
-	 float media = somma / 9.0;
-     printf("Elemento [%d][%d]: %f -> Media 3x3: %f\n", i, j, matrice[i][j], media);
-}
-
-float somma_intorno_prima(float **matrice, float somma_prima, int i, int j) {
-	float somma = 0;
-	for (int ik = -1; ik < 2; ik++) for (int jk = -1; jk < 2; jk++) somma += matrice[i + ik][j + jk];
-	return somma;
-}
-
-float somma_intorno_scorrimento(float **matrice, float somma_prima, int i_prima, int j_prima, int i_dopo, int j_dopo) {
-    float somma = somma_prima;
-    int direzione = j_dopo - j_prima;
-    if (direzione) {
-    	for (int k = -1; k < 2; k++) somma -= matrice[i_prima + k][j_prima - 1 * direzione];
-    	for (int k = -1; k < 2; k++) somma += matrice[i_dopo + k][j_dopo + 1 * direzione];
+float shift_sum_adj(float **m, float old_sum, int i_from, int j_from, int i_to, int j_to) {
+    float sum = old_sum;
+    int dir = j_to - j_from;
+    if (dir) {
+    	for (int k = -1; k < 2; k++) sum -= m[i_from + k][j_from - 1 * dir];
+    	for (int k = -1; k < 2; k++) sum += m[i_to + k][j_to + 1 * dir];
     } else {
-    	for (int k = -1; k < 2; k++) somma -= matrice[i_prima - 1][j_prima + k];
-    	for (int k = -1; k < 2; k++) somma += matrice[i_dopo + 1][j_dopo + k];
+    	for (int k = -1; k < 2; k++) sum -= m[i_from - 1][j_from + k];
+    	for (int k = -1; k < 2; k++) sum += m[i_to + 1][j_to + k];
     }
-    return somma;
-}
-
-void media_intorno_processo(float **matrice, int righe, int colonne) {
-	float somma = 0.0;
-    for (int i = 1; i < righe - 1; i++) {
-        if (i % 2 == 1) for (int j = 1; j < colonne - 1; j++) {
-        	if (i == 1 && j == 1) somma = somma_intorno_prima(matrice, somma, i, j);
-			else if (j == 1) somma = somma_intorno_scorrimento(matrice, somma, i - 1, j, i, j);
-			else somma = somma_intorno_scorrimento(matrice, somma, i, j - 1, i, j);
-			conferma_media(matrice, somma, i, j);
-        } else for (int j = colonne - 2; j > 0; j--) {
-		    if (j == colonne - 2) somma = somma_intorno_scorrimento(matrice, somma, i - 1, j, i, j);
-			else somma = somma_intorno_scorrimento(matrice, somma, i, j + 1, i, j);
-			conferma_media(matrice, somma, i, j);
-        }
-    }
+    return sum;
 }
 
 int main(int argc, char *argv[]) {
@@ -82,76 +48,63 @@ int main(int argc, char *argv[]) {
         printf("Utilizzo: %s <dimensione>\n", argv[0]);
         return 1;
     }
-    int dimensione = atoi(argv[1]);
-    if (dimensione <= 0) {
+    int dim = atoi(argv[1]);
+    int dim_res = dim / 2;
+    int dim_frm = dim_res + 2;
+    if (dim <= 0) {
         printf("La dimensione deve essere un numero intero positivo.\n");
         return 1;
     }
     
-    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
-        printf("Errore in MPI_Init\n");
-        return 1;
-    }
+    double start = omp_get_time();
     
-    int size, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	int cols = size > dim_res ? dim_res : sqrt(size);
+	int dim_col = dim_res / cols;
+	int big_cols = dim_res % cols;
+	int rows = size / cols > dim_res ? dim_res : size / cols;
+	int dim_row = dim_res / rows;
+	int big_rows = dim_res % rows;
+	
+	float **m = create_matrix(dim_frm);
+    float **r = create_matrix(dim_res);
+    int size = omp_get_num_threads();
+	omp_set_nested(1);
     
-    printf("[Sono il processo %d di %d]\n", my_rank, size);
-    
-    int colonne, dimensione_colonna, colonne_grandi;
-    int righe, dimensione_righe, righe_grandi;
-    
-    if (rank == 0) {
-		int dimensione_risultato = dimensione / 2;
-		int dimensione_cornice= dimensione_risultato + 2;
-		float **A = alloca_inizializza_matrice(dimensione);
-
-		printf("Matrice in alto a sinistra %dx%d con bordo aggiunto:\n", dimensione_risultato, dimensione_risultato);
-		stampa_matrice(A, dimensione_cornice);
+    #pragma omp parallel num_threads(cols) shared(m, r) {
+		int rank = omp_get_thread_num();
+    	int shift = rank * (dim_col + 1);
+    	if (rank > big_cols) shift += (rank - big_cols) * dim_col;
+    	int j_from = 1 + shift;
+    	int j_to = (big_cols > rank ? 0 : 1) + dim_col + shift;
+		#pragma omp parallel num_threads(rows) shared(j_from, j_to, m, r) {
+			int rank = omp_get_thread_num();
+    		int shift = rank * (dim_row + 1);
+    		if (rank > big_rows) shift += (rank - big_rows) * dim_row;
+    		int i_from = 1 + shift;
+    		int i_to = (big_rows > rank ? 0 : 1) + dim_row + shift;
 		
-		colonne = size > dimensione_risultato ? dimensione_risultato : sqrt(size);
-	 	dimensione_colonna = dimensione_risultato / colonne;
-		colonne_grandi = dimensione_risultato % colonne;
-		righe = size / colonne > dimensione_risultato ? dimensione_risultato : size / colonne;
-		dimensione_riga = dimensione_risultato / righe;
-	   	righe_grandi = dimensione_risultato % righe;
-		
-		printf("Colonne: %d %d %d\n", colonne, dimensione_colonna, colonne_grandi);
-		printf("Righe: %d %d %d\n", righe, dimensione_riga, righe_grandi);
-		
-		MPI_Scatter(A, elementi, MPI_FLOAT, &partizioneA, elementi, MPI_FLOAT, 0, MPI_COMM_WORLD);
-		
-    }
-    else if (rank < colonne) {
-    	if (rank < colonne_grandi) {
-    		elementi = (dimensione_colonna + 1) * dimensione_cornice;
-    		scorrimento = (rank - 1) * (dimensione_colonna + 1);
-    	} else {
-    		elementi = dimensione_colonna * dimensione_cornice;
-    		scorrimento = colonne_grandi * (dimensione_colonna + 1) + (rank - 1 - colonne_grandi) * dimensione_colonna;
-    	}
-    	float **partizioneA = dividi_matrice(A, 1, dimensione_risultato, 1 + scorrimento, dimensione_colonna + scorrimento);
-    	MPI_Scatter(NULL, elementi, MPI_FLOAT, &partizioneA, elementi, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    }
-    if () {
-    
+			float sum = 0.0;
+			for (int i = i_from - 1; i <= i_to + 1; i++) {
+				if (i % 2) for (int j = j_from - 1; j <= j_to + 1; j++) {
+					if (i == 1 && j == 1) sum = first_sum_adj(m, i, j);
+					else if (j == 1) sum = shift_sum_adj(m, sum, i - 1, j, i, j);
+					else sum = shift_sum_adj(m, sum, i, j - 1, i, j);
+					r[i - i_from + 1][j - j_from + 1] = sum / 9.0;
+				} else for (int j = j_to - 1; j <= j_from + 1; j++) {
+					if (j == cols - 2) sum = shift_sum_adj(m, sum, i - 1, j, i, j);
+					else sum = shift_sum_adj(m, sum, i, j + 1, i, j);
+					r[i - i_from + 1][j - j_from + 1] = sum / 9.0;
+				}
+			}
+		}
     }
     
+    show_matrix(r, dim_res);
+    free_matrix(m, dim_res + 2)
+    free_matrix(r, dim_res)
     
-    dividi_matrice(A, 1, 1, 1, 1); 
-    
-    media_intorno_processo(A, dimensione_risultato + 2, dimensione_risultato + 2);
-    
-    if (MPI_Finalize() != MPI_SUCCESS) {
-        printf("Errore in MPI_Finalize\n");
-        return 1;
-    }
-    
-    dealloca_matrice(A, dimensione_risultato + 2);
-    
-    for (int i = 0; i < i_fine - i_inizio + 3; i++) free(divisa[i]);
-    free(divisa);
+    double end = omp_get_time();
+    printf("Dimensione Matrice: %d\nNumero Processi: %d\nTempo Impiegato: %f\n", dim, size, end - start);
 
     return 0;
 }
