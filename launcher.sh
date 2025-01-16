b6df8394c12d8e2ef059969fd760c5427ad89a18
Stefano Casini <stefa@Huawei-Ste>
2025-01-16 10:21:18 +0100

    Create Launcher File

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --account=tra24_IngInfBO
+#SBATCH --partition=g100_usr_prod
+#SBATCH --nodes=5
+#SBATCH --ntasks-per-node=1
+#SBATCH -o job.out
+#SBATCH -e job.err
+module load autoload intelmpi
+srun ./MPI 5
