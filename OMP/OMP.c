72aa322dc772413a5d6068e0ac13b3aa9527196a
Stefano Casini <stefa@Huawei-Ste>
2025-02-03 09:54:43 +0100

    OMP big fixed

--- OMP/OMP.c
+++ OMP/OMP.c
@@ -111,56 +111,67 @@ int main(int argc, char **argv)
     printf("big_threadsInCol: %d\n", big_threadsInCol);
 
     omp_set_nested(1);
-#pragma omp parallel num_threads(cols) shared(m, r, dim_col, big_cols, big_threadsInCol, threadsInCol)
+#pragma omp parallel num_threads(cols) shared(m, r, dim_col, big_cols, big_threadsInCol, threadsInCol, dim_res)
     {
         int rank = omp_get_thread_num();
-        int subthreads = rank > big_threadsInCol ? threadsInCol + 1 : threadsInCol;
+        int subthreads = (rank < big_threadsInCol && big_threadsInCol != 0) ? threadsInCol + 1 : threadsInCol;
 
         int shift = rank * (dim_col);
-        if (rank > big_cols && big_cols != 0)
+        if (rank >= big_cols && big_cols != 0)
             shift += (rank - big_cols) * dim_col;
         int j_from = 1 + shift;
         int j_to = (rank > big_cols && big_cols != 0 ? 1 : 0) + dim_col + shift;
-
-        printf("[RANK %d]: %d, %d, %d\n", rank, j_from, j_to, shift);
-
+        printf("[RANK %d] : Subthreads %d\n", rank, subthreads);
 // Nested pragma
-#pragma omp parallel num_threads(subthreads) shared(j_from, j_to, m, r, big_threadsInCol, subthreads)
+#pragma omp parallel num_threads(subthreads) shared(j_from, j_to, m, r, big_threadsInCol, dim_res) firstprivate(subthreads)
         {
             int nst_rank = omp_get_thread_num();
-            int nst_shift = nst_rank * (big_threadsInCol + 1);
-            if (nst_rank > big_threadsInCol)
-                nst_shift += (nst_rank - big_threadsInCol) * subthreads;
+            // int nst_shift = nst_rank * (big_threadsInCol + 1);
+            // if (nst_rank >= big_threadsInCol && big_threadsInCol!=0)
+            //     nst_shift += (nst_rank - big_threadsInCol) * subthreads;
+            // int i_from = 1 + nst_shift;
+            // int i_to = (nst_rank >= big_threadsInCol && big_cols != 0 ? 1 : 0) + subthreads + nst_shift;
+
+            int dim_rows = dim_res / subthreads;
+            int big_rows = dim_res % subthreads;
+            int nst_shift = nst_rank * (dim_rows);
+            if (nst_rank < big_rows && big_rows != 0)
+                nst_shift += (rank - big_rows) * dim_rows;
             int i_from = 1 + nst_shift;
-            int i_to = (big_threadsInCol > nst_rank ? 0 : 1) + subthreads + nst_shift;
+            int i_to = (nst_rank > big_rows && big_rows != 0 ? 1 : 0) + dim_rows + nst_shift;
+
+            printf("[RANK %d]: %d, %d, %d, %d\n", rank, j_from, j_to, i_from, i_to);
 
             float sum = 0.0;
-            for (int i = i_from - 1; i <= i_to + 1; i++)
+            for (int i = i_from; i <= i_to; i++)
             {
-                if (i % 2)
-                    for (int j = j_from - 1; j <= j_to + 1; j++)
+                if (((i - i_from) % 2) == 0)
+                {
+                    for (int j = j_from; j <= j_to; j++)
                     {
-                        if (i == 1 && j == 1)
-                            sum = first_sum_adj(m, i, j);
-                        else if (j == 1)
+                        if (i == i_from && j == j_from)
+                            sum = first_sum_adj(m, i_from, j_from);
+                        else if (j == j_from)
                             sum = shift_sum_adj(m, sum, i - 1, j, i, j);
                         else
                             sum = shift_sum_adj(m, sum, i, j - 1, i, j);
-                        r[i - i_from + 1][j - j_from + 1] = sum / 9.0;
+                        r[i - 1][j - 1] = sum / 9.0;
                     }
+                }
                 else
-                    for (int j = j_to - 1; j <= j_from + 1; j++)
+                    for (int j = j_to; j >= j_from; j--)
                     {
-                        if (j == cols - 2)
+                        if (j == j_to)
                             sum = shift_sum_adj(m, sum, i - 1, j, i, j);
                         else
                             sum = shift_sum_adj(m, sum, i, j + 1, i, j);
-                        r[i - i_from + 1][j - j_from + 1] = sum / 9.0;
+                        r[i - 1][j - 1] = sum / 9.0;
                     }
             }
         }
     }
 
+    show_matrix(m, dim_frm);
     show_matrix(r, dim_res);
     free_matrix(m, dim_frm);
     free_matrix(r, dim_res);
